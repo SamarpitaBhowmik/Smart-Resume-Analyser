1e1af473c38f374688b6cd0e5dbfc6c07fc81ede
Samarpita Bhowmik <samarpita.bh2@gmail.com>
2025-09-03 22:51:07 +0530

    Initialize backend: Express + MongoDB setup

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "resume-analyser", 
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
+connectDB();
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Backend running...");
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, () => {
+  console.log(`Server running on port ${PORT}`);
+});
